f127b41d3f54c8cfafd1ac69a714381d199851e1
hafidz_Jvars(062) <hafidzexzty@gmail.com>
2024-07-25 17:07:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const biodataForm = document.getElementById('biodata-form');
+const friendsList = document.getElementById('friends-list');
+const addFriendBtn = document.getElementById('add-friend-btn');
+
+let biodata = {};
+let friends = [];
+
+biodataForm.addEventListener
